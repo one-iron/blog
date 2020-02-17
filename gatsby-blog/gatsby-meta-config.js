0e450fa4a963989b054d1f974e3383c6f5ff2157
one-iron <lee.oneiron@gamil.com>
2020-02-17 11:47:00 +0900

    first commit

---
+++ gatsby-blog/gatsby-meta-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `One-Iron`,
+  description: `Blog posted about ...`,
+  author: `[원철]`,
+  introduction: `🏃‍♂️ “넘어지면 그냥 일어나지 말고 무언가를 주워라”`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/one-iron`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'One-Iron', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
