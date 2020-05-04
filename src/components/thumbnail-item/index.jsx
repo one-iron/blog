d2aa1abbfd609e79e27561033c6ced2759670bac
oneiron <admin@adminui-MacBookPro.local>
2020-05-05 01:50:20 +0900

    modify blog layout

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -3,12 +3,32 @@ import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
 
 import './index.scss'
-
-export const ThumbnailItem = ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-    </div>
-  </Link>
-)
+export const ThumbnailItem = ({ node }) => {
+  let date = node.frontmatter.date
+  let year = date.slice(-4)
+  let month = date.slice(0, date.indexOf(' '))
+  let monthChange = {
+    January: '01월',
+    February: '02월',
+    March: '03월',
+    April: '04월',
+    May: '05월',
+    June: '06월',
+    July: '07월',
+    August: '08월',
+    September: '09월',
+    October: '10월',
+    November: '11월',
+    December: '12월',
+  }
+  let day = date.slice(date.indexOf(' ') + 1, date.indexOf(' ') + 3)
+  return (
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
+      <div key={node.fields.slug}>
+        <h3>{node.frontmatter.title || node.fields.slug}</h3>
+        <p>{`${year}년${monthChange[month]}${day}일`}</p>
+        <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </div>
+    </Link>
+  )
+}
